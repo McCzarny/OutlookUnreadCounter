1c3f26825558db9b1e7a81eef520e984d44c4c85
Maciej Czarnecki <mcczarny@gmail.com>
2024-12-14 20:56:43 +0100

    Change second row to extra info and allow to include both information to have 3 rows in total

--- com.mcczarny.outlookunreadcounter.sdPlugin/code/main.py
+++ com.mcczarny.outlookunreadcounter.sdPlugin/code/main.py
@@ -10,10 +10,11 @@ class MailStates(IntEnum):
     READ = 0
     UNREAD = 1
 
-class SecondRowStates(str, Enum):
+class ExtraInfoStates(str, Enum):
     NONE = "None"
     SENDER = "Sender"
     SUBJECT = "Subject"
+    BOTH = "Both"
 
 class UnreadCounter(Action):
 
@@ -21,8 +22,9 @@ class UnreadCounter(Action):
     MAIL_COUNT_UPDATE_INTERVAL: float = 10
     ACCOUNT_KEY = "account"
     ACCOUNTS_KEY = "accounts"
-    SECOND_ROW_KEY = "second_row"
-    SECOND_ROW_STATES_KEY = "second_row_states"
+    EXTRA_INFO_KEY = "extra_info"
+    EXTRA_INFO_STATES_KEY = "extra_info_states"
+    EXTRA_INFO_MAX_LENGTH = 10
 
     wake_event = threading.Event()
 
@@ -30,7 +32,7 @@ class UnreadCounter(Action):
     monitor_outlook = None
     context = ""
     context_to_account = {}
-    context_to_second_row = {}
+    context_to_extra_info = {}
 
     def set_accounts_settings(self, context: str, settings: dict):
         logger.debug(f"[{context}] set_accounts_settings: {settings}")
@@ -40,24 +42,24 @@ class UnreadCounter(Action):
             logger.debug(f"[{context}] Using account from the settings: {settings.get(self.ACCOUNT_KEY)}")
             self.context_to_account[context] = settings.get(self.ACCOUNT_KEY)
         
-        if self.SECOND_ROW_KEY in settings:
-            logger.debug(f"[{context}] Using second_row_state from the settings: {settings.get(self.SECOND_ROW_KEY)}")
-            self.context_to_second_row[context] = settings.get(self.SECOND_ROW_KEY)
+        if self.EXTRA_INFO_KEY in settings:
+            logger.debug(f"[{context}] Using {self.EXTRA_INFO_KEY} from the settings: {settings.get(self.EXTRA_INFO_KEY)}")
+            self.context_to_extra_info[context] = settings.get(self.EXTRA_INFO_KEY)
 
         if context not in self.context_to_account or self.context_to_account[context] not in accounts:
             self.context_to_account[context] = accounts[0] if len(accounts) > 0 else ""
             logger.debug(f"[{context}] Setting account to: {self.context_to_account[context]}")
         
-        if context not in self.context_to_second_row or self.context_to_second_row[context] not in [state.value for state in SecondRowStates]:
+        if context not in self.context_to_extra_info or self.context_to_extra_info[context] not in [state.value for state in ExtraInfoStates]:
             logger.debug(
-                f"[{context}] Setting second_row_state to: {self.context_to_second_row[context]}."
-                + f"Old value: {self.context_to_second_row.get(context)}")
-            self.context_to_second_row[context] = SecondRowStates.NONE
+                f"[{context}] Setting {self.EXTRA_INFO_KEY} to: {ExtraInfoStates.NONE}."
+                + f"Old value: {self.context_to_extra_info.get(context)}")
+            self.context_to_extra_info[context] = ExtraInfoStates.NONE
         payload = {
             self.ACCOUNT_KEY: self.context_to_account[context],
             self.ACCOUNTS_KEY: accounts,
-            self.SECOND_ROW_KEY: self.context_to_second_row[context],
-            self.SECOND_ROW_STATES_KEY: [state.value for state in SecondRowStates],
+            self.EXTRA_INFO_KEY: self.context_to_extra_info[context],
+            self.EXTRA_INFO_STATES_KEY: [state.value for state in ExtraInfoStates],
         }
         self.set_settings(context=context, payload=payload)
     @log_errors
@@ -68,29 +70,43 @@ class UnreadCounter(Action):
     def update_unread_count(self, outlook, context: str, account: str):
         logger.debug(f"update_unread_count: {context} account: {account}")
         unread_count = "?"
-        second_row = ""
+        extra_info = ""
         folder = outlook.Stores(account).GetDefaultFolder(6) if account else outlook.GetDefaultFolder(6)
 
         logger.debug(f"account: {account}")
         unread_count = folder.UnReadItemCount
         if unread_count > 0:
-            logger.debug(f"Unread count is bigger than 0, processing second row...")
+            logger.debug(f"Unread count is bigger than 0, processing extra info...")
             try:
-                if self.context_to_second_row[context] == SecondRowStates.SENDER:
+                last_unread_email = folder.Items.Restrict('[UnRead] = True').GetLast()
+                if self.context_to_extra_info[context] == ExtraInfoStates.SENDER \
+                    or self.context_to_extra_info[context] == ExtraInfoStates.BOTH:
                     logger.debug(f"sender")
-                    second_row = folder.Items(1).SenderName
-                elif self.context_to_second_row[context] == SecondRowStates.SUBJECT:
+                    sender_name = last_unread_email.SenderName
+                    if len(sender_name) > self.EXTRA_INFO_MAX_LENGTH:
+                        logger.debug(f"sender name ({sender_name}) is too long, truncating...")
+                        sender_name = sender_name[:self.EXTRA_INFO_MAX_LENGTH - 3] + "..."
+                    extra_info = sender_name
+                if self.context_to_extra_info[context] == ExtraInfoStates.SUBJECT \
+                    or self.context_to_extra_info[context] == ExtraInfoStates.BOTH:
                     logger.debug(f"subject")
-                    second_row = folder.Items(1).Subject
-            except:
-                logger.error(f"Error getting second row")
-            logger.debug(f"second_row: [{second_row}]")
+                    if extra_info != "":
+                        extra_info += "\n"
+                    subject = last_unread_email.Subject
+                    if len(subject) > self.EXTRA_INFO_MAX_LENGTH:
+                        logger.debug(f"subject ({subject}) is too long, truncating...")
+                        subject = subject[:self.EXTRA_INFO_MAX_LENGTH - 3] + "..."
+                    extra_info += subject
+            except Exception as e:
+                logger.error(f"Error getting extra info: {e}")
+            logger.debug(f"extra_info: [{extra_info}]")
         state = MailStates.UNREAD if unread_count > 0 else MailStates.READ
-        logger.debug(f"unread_count: {unread_count} second_row: {second_row} state: {state}")
+        logger.debug(f"unread_count: {unread_count} extra_info: {extra_info} state: {state}")
         self.set_state(context=context, state=state)
 
-        title_content = f"{unread_count}" if second_row == "" else f"{unread_count}\n{second_row}"
-        self.set_title(context=context, title=f"{title_content}")
+        title_content = f"{unread_count}" if extra_info == "" else f"{unread_count}\n{extra_info}"
+        logger.debug(f"title_content: {title_content}")
+        self.set_title(context=context, title=title_content)
 
     @log_errors
     def on_did_receive_settings(self, obj: events_received_objs.DidReceiveSettings):
@@ -100,8 +116,8 @@ class UnreadCounter(Action):
             self.context_to_account[obj.context] = obj.payload.settings.get(self.ACCOUNT_KEY)
             update_tiles = True
         
-        if obj.payload.settings.get(self.SECOND_ROW_KEY):
-            self.context_to_second_row[obj.context] = obj.payload.settings.get(self.SECOND_ROW_KEY)
+        if obj.payload.settings.get(self.EXTRA_INFO_KEY):
+            self.context_to_extra_info[obj.context] = obj.payload.settings.get(self.EXTRA_INFO_KEY)
             update_tiles = True
 
         if update_tiles:
