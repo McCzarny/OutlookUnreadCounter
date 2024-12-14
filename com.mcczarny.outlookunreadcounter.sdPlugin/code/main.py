5b6117fa9ff92417b6a387a0d059322963e4a6b7
Maciej Czarnecki <mcczarny@gmail.com>
2024-12-14 13:12:35 +0100

    Settings cleanup and add second row - Major cleanup in settings - both, on the front and back - Labels in property inspector - Added option to specify if display something on the 2nd row (None, Subject, Sender)

--- com.mcczarny.outlookunreadcounter.sdPlugin/code/main.py
+++ com.mcczarny.outlookunreadcounter.sdPlugin/code/main.py
@@ -1,7 +1,6 @@
 import settings
 import threading
-import time
-from enum import IntEnum
+from enum import IntEnum, Enum
 
 from streamdeck_sdk import StreamDeck, Action, events_received_objs, logger, log_errors, in_separate_thread
 import win32com.client
@@ -11,31 +10,56 @@ class MailStates(IntEnum):
     READ = 0
     UNREAD = 1
 
+class SecondRowStates(str, Enum):
+    NONE = "None"
+    SENDER = "Sender"
+    SUBJECT = "Subject"
 
 class UnreadCounter(Action):
 
     UUID = "com.mcczarny.outlookunreadcounter.unreadcounter"
     MAIL_COUNT_UPDATE_INTERVAL: float = 10
+    ACCOUNT_KEY = "account"
+    ACCOUNTS_KEY = "accounts"
+    SECOND_ROW_KEY = "second_row"
+    SECOND_ROW_STATES_KEY = "second_row_states"
+
     wake_event = threading.Event()
 
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
     monitor_outlook = None
     context = ""
     context_to_account = {}
+    context_to_second_row = {}
 
     def set_accounts_settings(self, context: str, settings: dict):
-        logger.debug(f"[{context}] set_accounts_settings")
+        logger.debug(f"[{context}] set_accounts_settings: {settings}")
         accounts = [acc.DisplayName for acc in self.outlook.Stores]
         logger.debug(f"[{context}] accounts: {accounts}")
-        if "account" in settings:
-            logger.debug(f"[{context}] Using account from the settings: {settings.get('account')}")
-            self.context_to_account[context] = settings.get("account")
+        if self.ACCOUNT_KEY in settings:
+            logger.debug(f"[{context}] Using account from the settings: {settings.get(self.ACCOUNT_KEY)}")
+            self.context_to_account[context] = settings.get(self.ACCOUNT_KEY)
+        
+        if self.SECOND_ROW_KEY in settings:
+            logger.debug(f"[{context}] Using second_row_state from the settings: {settings.get(self.SECOND_ROW_KEY)}")
+            self.context_to_second_row[context] = settings.get(self.SECOND_ROW_KEY)
 
         if context not in self.context_to_account or self.context_to_account[context] not in accounts:
             self.context_to_account[context] = accounts[0] if len(accounts) > 0 else ""
             logger.debug(f"[{context}] Setting account to: {self.context_to_account[context]}")
-        self.set_settings(context=context, payload={"account": self.context_to_account[context], "accounts": accounts})
-
+        
+        if context not in self.context_to_second_row or self.context_to_second_row[context] not in [state.value for state in SecondRowStates]:
+            logger.debug(
+                f"[{context}] Setting second_row_state to: {self.context_to_second_row[context]}."
+                + f"Old value: {self.context_to_second_row.get(context)}")
+            self.context_to_second_row[context] = SecondRowStates.NONE
+        payload = {
+            self.ACCOUNT_KEY: self.context_to_account[context],
+            self.ACCOUNTS_KEY: accounts,
+            self.SECOND_ROW_KEY: self.context_to_second_row[context],
+            self.SECOND_ROW_STATES_KEY: [state.value for state in SecondRowStates],
+        }
+        self.set_settings(context=context, payload=payload)
     @log_errors
     def on_will_appear(self, obj: events_received_objs.WillAppear):
         logger.debug(f"on_will_appear: {obj.context}")
@@ -44,22 +68,43 @@ class UnreadCounter(Action):
     def update_unread_count(self, outlook, context: str, account: str):
         logger.debug(f"update_unread_count: {context} account: {account}")
         unread_count = "?"
-        if account:
-            logger.debug(f"account: {account}")
-            unread_count = outlook.Stores(account).GetDefaultFolder(6).UnReadItemCount
-        else:
-            logger.debug(f"default folder")
-            unread_count = outlook.GetDefaultFolder(6).UnReadItemCount
+        second_row = ""
+        folder = outlook.Stores(account).GetDefaultFolder(6) if account else outlook.GetDefaultFolder(6)
+
+        logger.debug(f"account: {account}")
+        unread_count = folder.UnReadItemCount
+        if unread_count > 0:
+            logger.debug(f"Unread count is bigger than 0, processing second row...")
+            try:
+                if self.context_to_second_row[context] == SecondRowStates.SENDER:
+                    logger.debug(f"sender")
+                    second_row = folder.Items(1).SenderName
+                elif self.context_to_second_row[context] == SecondRowStates.SUBJECT:
+                    logger.debug(f"subject")
+                    second_row = folder.Items(1).Subject
+            except:
+                logger.error(f"Error getting second row")
+            logger.debug(f"second_row: [{second_row}]")
         state = MailStates.UNREAD if unread_count > 0 else MailStates.READ
-        logger.debug(f"unread_count: {unread_count} state: {state}")
+        logger.debug(f"unread_count: {unread_count} second_row: {second_row} state: {state}")
         self.set_state(context=context, state=state)
-        self.set_title(context=context, title=f"{unread_count}")
+
+        title_content = f"{unread_count}" if second_row == "" else f"{unread_count}\n{second_row}"
+        self.set_title(context=context, title=f"{title_content}")
 
     @log_errors
     def on_did_receive_settings(self, obj: events_received_objs.DidReceiveSettings):
         logger.debug(f"on_did_receive_settings: {obj.payload}")
-        if obj.payload.settings.get("account"):
-            self.context_to_account[obj.context] = obj.payload.settings.get("account")
+        update_tiles = False
+        if obj.payload.settings.get(self.ACCOUNT_KEY):
+            self.context_to_account[obj.context] = obj.payload.settings.get(self.ACCOUNT_KEY)
+            update_tiles = True
+        
+        if obj.payload.settings.get(self.SECOND_ROW_KEY):
+            self.context_to_second_row[obj.context] = obj.payload.settings.get(self.SECOND_ROW_KEY)
+            update_tiles = True
+
+        if update_tiles:
             self.wake_event.set()
 
     @log_errors
